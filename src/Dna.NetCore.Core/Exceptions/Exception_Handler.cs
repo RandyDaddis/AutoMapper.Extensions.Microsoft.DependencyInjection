b1e5d663c0cec8dd86ad68566273bd9d84d2eb7f
Randy Daddis <randy.daddis@outlook.com>
2016-10-18 11:02:11 -0400

    target NET462 TFM for  System.Exception.TargetSite related code

--- src/Dna.NetCore.Core/Exceptions/Exception_Handler.cs
+++ src/Dna.NetCore.Core/Exceptions/Exception_Handler.cs
@@ -10,48 +10,50 @@ namespace Dna.NetCore.Core.Exceptions
     {
         #region Methods
 
-        // TODO: .NET Core implementation
-        /////// <summary>
-        ///////// 
-        ///////// // WARNING: System.Exception.TargetSite is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
-        ///////// //                                                            .nuget\packages\System.Runtime\4.1.0\ref\netstandard1.5\System.Runtime.dll
-        /////////
-        /////// parse the messages and log the error
-        /////// </summary>
-        /////// <param name="exception"></param>
-        /////// <returns>CustomMessage</returns>
-        //public static CustomMessage Handle(this Exception exception)
-        //{
-        //    CustomMessage customMessage = new CustomMessage() { MessageDictionary1 = new Dictionary<string, string>(), MessageDictionary2 = new Dictionary<string, string>() };
+#if NET462
+         TODO: .NET Core implementation
+        ///// <summary>
+        /////// 
+        /////// // WARNING: System.Exception.TargetSite is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+        /////// //                                                            .nuget\packages\System.Runtime\4.1.0\ref\netstandard1.5\System.Runtime.dll
+        ///////
+        ///// parse the messages and log the error
+        ///// </summary>
+        ///// <param name="exception"></param>
+        ///// <returns>CustomMessage</returns>
+        public static CustomMessage Handle(this Exception exception)
+        {
+            CustomMessage customMessage = new CustomMessage() { MessageDictionary1 = new Dictionary<string, string>(), MessageDictionary2 = new Dictionary<string, string>() };
 
-        //    customMessage.Message = Get_StackTrace(exception);
+            customMessage.Message = Get_StackTrace(exception);
 
-        //    customMessage = ParseData(exception, customMessage);
+            customMessage = ParseData(exception, customMessage);
 
-        //    customMessage = ParseInnerException(exception, customMessage);
+            customMessage = ParseInnerException(exception, customMessage);
 
-        //    string message = " -->>Exception_HResult: "
-        //                        + exception.HResult.ToString()
-        //                        + " DeclaringType_ClassName: "
-        //                        + Get_TargetSite_DeclaringType_ClassName(exception)
-        //                        + " DeclaringType_MethodName: "
-        //                        + Get_TargetSite_MethodName(exception)
-        //                        //+ " DeclaringType_MemberType: "
-        //                        //+ Get_TargetSite_MemberType(exception)
-        //                        + " "
-        //                        + exception.Message ?? ""
-        //                        + " "
-        //                        + customMessage.Message ?? "";
+            string message = " -->>Exception_HResult: "
+                                + exception.HResult.ToString()
+                                + " DeclaringType_ClassName: "
+                                + Get_TargetSite_DeclaringType_ClassName(exception)
+                                + " DeclaringType_MethodName: "
+                                + Get_TargetSite_MethodName(exception)
+                                //+ " DeclaringType_MemberType: "
+                                //+ Get_TargetSite_MemberType(exception)
+                                + " "
+                                + exception.Message ?? ""
+                                + " "
+                                + customMessage.Message ?? "";
 
-        //    customMessage.Message += message;
+            customMessage.Message += message;
 
-        //    customMessage.IsErrorCondition = true;
+            customMessage.IsErrorCondition = true;
 
-        //    if (customMessage != null && !string.IsNullOrEmpty(customMessage.Message))
-        //        Log.Write(message);
+            if (customMessage != null && !string.IsNullOrEmpty(customMessage.Message))
+                Log.Write(message);
 
-        //    return customMessage;
-        //}
+            return customMessage;
+        }
+#endif
 
         public static CustomMessage ParseData(this Exception exception, CustomMessage customMessage)
         {
@@ -98,49 +100,52 @@ namespace Dna.NetCore.Core.Exceptions
             return stackTrace;
         }
 
-        // TODO: .NET Core implementation
-        /////// <summary>
+#if NET462
+
+         TODO: .NET Core implementation
+        ///// <summary>
+        ///// 
+        ///// // WARNING: System.Exception.TargetSite is implemented in mscorlib,
+        /////               but it is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+        ///// //                                                            .nuget\packages\System.Runtime\4.1.0\ref\netstandard1.5\System.Runtime.dll
+        /////
+        ///// returns the class name for the method that caused the exception
+        ///// </summary>
+        ///// <param name="exception"></param>
+        ///// <returns>class name</returns>
+        public static string Get_TargetSite_DeclaringType_ClassName(this Exception exception)
+        {
+            string name = exception.TargetSite.DeclaringType.FullName;
+            return name;
+        }
+
+        ///// <summary>
         /////// 
-        /////// // WARNING: System.Exception.TargetSite is implemented in mscorlib,
-        ///////               but it is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+        /////// // WARNING: System.Exception.TargetSite is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
         /////// //                                                            .nuget\packages\System.Runtime\4.1.0\ref\netstandard1.5\System.Runtime.dll
         ///////
-        /////// returns the class name for the method that caused the exception
-        /////// </summary>
-        /////// <param name="exception"></param>
-        /////// <returns>class name</returns>
-        //public static string Get_TargetSite_DeclaringType_ClassName(this Exception exception)
+        ///// returns the method name that caused the exception
+        ///// </summary>
+        ///// <param name="exception"></param>
+        ///// <returns>method name</returns>
+        public static string Get_TargetSite_MethodName(this Exception exception)
+        {
+            string name = exception.TargetSite.Name;
+            return name;
+        }
+
+        ///// <summary>
+        ///// identifies the member type (E.G. property vs. method) that caused the exception
+        ///// </summary>
+        ///// <param name="exception"></param>
+        ///// <returns>member types:Constructor, Event, Field, Method, Property, TypeInfo, Custom, NestedType, All</returns>
+        //public static string Get_TargetSite_MemberType(this Exception exception)
         //{
-        //    string name = exception.TargetSite.DeclaringType.FullName;
-        //    return name;
+        //    string s = System.Enum.GetName(MemberTypes, exception.TargetSite.MemberType.GetType as object);
+
+        //    return s;
         //}
-
-        /////// <summary>
-        ///////// 
-        ///////// // WARNING: System.Exception.TargetSite is not implemented in System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
-        ///////// //                                                            .nuget\packages\System.Runtime\4.1.0\ref\netstandard1.5\System.Runtime.dll
-        /////////
-        /////// returns the method name that caused the exception
-        /////// </summary>
-        /////// <param name="exception"></param>
-        /////// <returns>method name</returns>
-        //public static string Get_TargetSite_MethodName(this Exception exception)
-        //{
-        //    string name = exception.TargetSite.Name;
-        //    return name;
-        //}
-
-        /////// <summary>
-        /////// identifies the member type (E.G. property vs. method) that caused the exception
-        /////// </summary>
-        /////// <param name="exception"></param>
-        /////// <returns>member types:Constructor, Event, Field, Method, Property, TypeInfo, Custom, NestedType, All</returns>
-        ////public static string Get_TargetSite_MemberType(this Exception exception)
-        ////{
-        ////    string s = System.Enum.GetName(MemberTypes, exception.TargetSite.MemberType.GetType as object);
-
-        ////    return s;
-        ////}
+#endif
 
         #endregion
     }
