4ae41d88b5045dbcde8b3edc0c1def7bf725805f
Randy Daddis <randy.daddis@outlook.com>
2016-10-19 09:58:08 -0400

    refactored Autofac configuration due to refactoring AutoMapper from BLL to DAL

--- src/Dna.NetCore.Core.BLL/AutofacModule_CoreBLL.cs
+++ src/Dna.NetCore.Core.BLL/AutofacModule_CoreBLL.cs
@@ -24,18 +24,6 @@ namespace Dna.NetCore.Core.BLL
         {
             var assembly = Assembly.Load(new AssemblyName("Dna.NetCore.Core.BLL"));
 
-            // ConfigurationProviderProxy.cs is no longer necessary. 
-            //   - I have refactored type mapping from AutoMapper.IMappingEngine to AutoMapper.AutoMapper
-            //   - AutoMapper.AutoMapper is not injected via DI.It is referenced directly in *_mapper.cs
-            //builder.RegisterType<ConfigurationProviderProxy>()
-            //    .As<IConfigurationProvider>()
-            //    .InstancePerLifetimeScope();
-
-            builder.RegisterAssemblyTypes(assembly)
-                .Where(t => t.Name.EndsWith("Mapper"))
-                .AsImplementedInterfaces()
-                .InstancePerLifetimeScope();
-
             builder.RegisterAssemblyTypes(assembly)
                 .Where(t => t.Name.EndsWith("Queries"))
                 .AsImplementedInterfaces()
---
+++ src/Dna.NetCore.Core.DAL.AutoMapper/AutofacModule_CoreDALAutoMapper.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Dna.NetCore.Core.DAL.AutoMapper
+{
+    public class AutofacModule_CoreDALAutoMapper : Autofac.Module
+    {
+        #region ctor
+
+        public AutofacModule_CoreDALAutoMapper()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            RegisterComponents(builder);
+        }
+
+        private static void RegisterComponents(ContainerBuilder builder)
+        {
+            var assembly = Assembly.Load(new AssemblyName("Dna.NetCore.Core.DAL.AutoMapper"));
+
+            // ConfigurationProviderProxy.cs is no longer necessary. 
+            //   - I have refactored type mapping from AutoMapper.IMappingEngine to AutoMapper.AutoMapper
+            //   - AutoMapper.AutoMapper is not injected via DI.It is referenced directly in *_mapper.cs
+            //builder.RegisterType<ConfigurationProviderProxy>()
+            //    .As<IConfigurationProvider>()
+            //    .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(t => t.Name.EndsWith("Mapper"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+        }
+
+        #endregion
+    }
+}
