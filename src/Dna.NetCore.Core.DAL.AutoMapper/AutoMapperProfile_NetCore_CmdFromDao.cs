6d9528ec9c1942146e93be975226dcf55d09bf39
Randy Daddis <randy.daddis@outlook.com>
2016-10-20 11:25:56 -0400

    added AutoMapper profiles

---
+++ src/Dna.NetCore.Core.DAL.AutoMapper/AutoMapperProfile_NetCore_CmdFromDao.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Dna.NetCore.Core.BLL.Commands.Common;
+using Dna.NetCore.Core.BLL.Commands.Localization;
+using Dna.NetCore.Core.BLL.Entities.Common;
+using Dna.NetCore.Core.BLL.Entities.Localization;
+
+namespace Dna.NetCore.Core.DAL.AutoMapper
+{
+    public class AutoMapperProfile_NetCore_CmdFromDao : Profile
+    {
+        #region ctor
+
+        public AutoMapperProfile_NetCore_CmdFromDao()
+        {
+            CreateMaps();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void CreateMaps()
+        {
+            CreateMap<AddressType, AddressTypeCmd>();
+            CreateMap<City, CityCmd>()
+            .ForMember(cmd => cmd.StateOrProvinceAbbreviation, opt => opt.Ignore())
+            .ForMember(cmd => cmd.StateOrProvinceSummaries, opt => opt.Ignore());
+            CreateMap<County, CountyCmd>()
+            .ForMember(cmd => cmd.StateOrProvinceAbbreviation, opt => opt.Ignore())
+            .ForMember(cmd => cmd.StateOrProvinceSummaries, opt => opt.Ignore());
+            CreateMap<CountyCity, CountyCityCmd>();
+            CreateMap<Country, CountryCmd>();
+            CreateMap<Currency, CurrencyCmd>();
+            CreateMap<ExchangeRate, ExchangeRateCmd>();
+            CreateMap<Locale, LocaleCmd>();
+            CreateMap<MimeType, MimeTypeCmd>();
+            CreateMap<MimeTypeGroup, MimeTypeGroupCmd>();
+            CreateMap<PersonType, PersonTypeCmd>();
+            CreateMap<PhoneNumberType, PhoneNumberTypeCmd>();
+            CreateMap<StateOrProvince, StateOrProvinceCmd>()
+            .ForMember(cmd => cmd.CountryAbbreviation, opt => opt.MapFrom(s => s.Country.Abbreviation));
+            CreateMap<Dna.NetCore.Core.BLL.Entities.Common.TimeZone, TimeZoneCmd>();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Dna.NetCore.Core.DAL.AutoMapper/AutoMapperProfile_NetCore_DaoFromCmd.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Dna.NetCore.Core.BLL.Commands.Common;
+using Dna.NetCore.Core.BLL.Commands.Localization;
+using Dna.NetCore.Core.BLL.Entities.Common;
+using Dna.NetCore.Core.BLL.Entities.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dna.NetCore.Core.DAL.AutoMapper
+{
+    public class AutoMapperProfile_NetCore_DaoFromCmd : Profile
+    {
+        #region ctor
+
+        public AutoMapperProfile_NetCore_DaoFromCmd()
+        {
+            CreateMaps();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void CreateMaps()
+        {
+            CreateMap<AddressTypeCmd, AddressType>();
+            CreateMap<CityCmd, City>();
+            CreateMap<CountyCmd, County>();
+            CreateMap<CountyCityCmd, CountyCity>();
+            CreateMap<CountryCmd, Country>();
+            CreateMap<CurrencyCmd, Currency>();
+            CreateMap<ExchangeRateCmd, ExchangeRate>();
+            CreateMap<LocaleCmd, Locale>();
+            CreateMap<MimeTypeCmd, MimeType>();
+            CreateMap<MimeTypeGroupCmd, MimeTypeGroup>();
+            CreateMap<PersonTypeCmd, PersonType>();
+            CreateMap<PhoneNumberTypeCmd, PhoneNumberType>();
+            CreateMap<StateOrProvinceCmd, StateOrProvince>();
+            CreateMap<TimeZoneCmd, Dna.NetCore.Core.BLL.Entities.Common.TimeZone>();
+        }
+
+        #endregion
+    }
+}
